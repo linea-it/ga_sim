a0465c0e31e4bbc9a14281f80dc68f499d51cc55
glaubervila <glauber.vila.verde@gmail.com>
2022-02-07 14:59:13 -0300

    A library for gawa simulations was created from the example JN. all functions were moved to the library and a new JN was created using the library.

---
+++ gawa_simulations/__init__.py
@@ -0,0 +1,25 @@
+from gawa_simulations.gawa import (
+    dist_ang,
+    get_av,
+    make_footprint,
+    d_star_real_cat,
+    IMF_,
+    apply_err,
+    faker_bin,
+    unc,
+    faker,
+    join_cat,
+    snr_estimate,
+    write_sim_clus_features,
+    split_output_hpx,
+    SplitFtpHPX,
+    radec2GCdist,
+    RemoveCloseStars,
+)
+from gawa_simulations.plot import (
+    unnamed_plot,
+    plot_ftp,
+    plots_ang_size,
+    plots_ref,
+    plot_err,
+)
